0957088a6c9a34009483543cfb222f1ec5d731ea
jone4865 <cowhddnjsdl0@naver.com>
2023-01-15 19:20:01 +0900

    레포 이동 get 연결

---
+++ pages/api/postQuestion.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  status: string;
+};
+
+export default function question(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const nodemailer = require("nodemailer");
+
+  const transporter = nodemailer.createTransport({
+    service: process.env.NEXT_PUBLIC_SEND_SITE,
+    auth: {
+      user: process.env.NEXT_PUBLIC_SEND_ADDRESS,
+      pass: process.env.NEXT_PUBLIC_SEND_PASSWORD,
+    },
+  });
+
+  if (req.body) {
+    const mailOptions = {
+      from: req.body.email,
+      to: process.env.NEXT_PUBLIC_SEND_ADDRESS,
+      subject: `${process.env.NEXT_PUBLIC_SURVICE_NAME} 문의 메일`,
+      html: `<p><div>문의 내용 : ${JSON.parse(req.body).content}</div>
+    <div>연락처 : ${JSON.parse(req.body).cellPhone}</div></p>`,
+    };
+
+    transporter.sendMail(mailOptions, (err: any) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ status: res.statusCode.toString() });
+      } else {
+        res.status(200).json({ status: res.statusCode.toString() });
+      }
+    });
+  }
+
+  res.status(200).json({ status: res.statusCode.toString() });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import Landing from '../components/Landing/Landing'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import Landing from "../components/Landing/Landing";
+import { useEffect } from "react";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const content = "dadwadwadwa";
+  const email = "wcaown@dawdawd.com";
+  const cellPhone = "313-3333-2133";
+
+  const Test = { content, email, cellPhone };
+
+  //post 요청
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch("/api/postQuestion", {
+          method: "POST",
+          body: JSON.stringify(Test),
+        })
+      ).json();
+    })();
+  }, []);
+
+  //get 요청
+  useEffect(() => {
+    (async () => {
+      const getData = await (
+        await fetch("/api/getData", {
+          method: "GET",
+        })
+      ).json();
+      console.log(getData && getData.data[0]);
+    })();
+  }, []);
+
   return (
     <>
       <Landing />
     </>
-  )
+  );
 }
